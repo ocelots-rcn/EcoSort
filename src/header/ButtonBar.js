836c022e7ff42a6b7a6a3593ca1de2a9cc643db5
waisaed <waisaed@umich.edu>
2024-09-05 15:03:23 -0400

    see how 'check your results' button looks in buttonbar

--- src/header/ButtonBar.js
+++ src/header/ButtonBar.js
@@ -1,19 +1,38 @@
 import React from 'react';
 
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button'
+import GradingIcon from '@mui/icons-material/Grading';
 
 import NewBinButton from './NewBinButton';
 import SelectLanguage from './SelectLanguage';
 import About from './About';
 import Grouping from './Grouping';
 
+import { useDataContext } from '../provider/DataProvider';
+import { useTranslationContext } from '../provider/TranslationProvider';
+
 const ButtonBar = () => {
+
+  const { checkGrouping } = useDataContext();
+  const { translation } = useTranslationContext();
 
   return <Box sx={{
     display: 'flex',
     flexDirection: 'row',
   }}>
     <NewBinButton />
+
+      <Button
+          variant="contained"
+          startIcon={<GradingIcon/>}
+          color="success"
+          sx={{marginLeft: '1rem'}}
+          onClick={() => checkGrouping()}
+        >
+          {translation['checkGrouping']}
+        </Button>
+
     <Box sx={{flex: 1}}/>
     <Grouping />
     <Box sx={{flex: 1}}/>
